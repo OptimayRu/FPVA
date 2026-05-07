03c768078f5a82b7446594829019de24e19b8078
OptimayRu <optimay.ru@gmail.com>
2026-05-07 21:41:50 +0500

    Add Yandex Metrika to theme

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,21 @@
 import DefaultTheme from 'vitepress/theme'
 import Contacts from './components/Contacts.vue'
 import './custom.css'
+import { yandexMetrika } from '@hywax/vitepress-yandex-metrika'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
+  enhanceApp({ app, ctx }) {
+    // Регистрируем компонент Contacts
     app.component('Contacts', Contacts)
-  }
+    
+    // Подключаем Яндекс Метрику (только в продакшене)
+    if (typeof window !== 'undefined' && import.meta.env.PROD) {
+      yandexMetrika({ app, ctx }, {
+        counter: {
+          id: 109107105,
+        },
+      })
+    }
+  },
 }
